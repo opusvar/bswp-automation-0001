cbddec742d4c3795abf9530e55c832b249ebaad1
opusvar <sprojdev@gmail.com>
2021-08-11 08:27:06 -0400

    Added project set-up for coinflipstreaks.py

---
+++ chapter_practice/coinflipstreaks.py
@@ -0,0 +1,21 @@
+
+# write a program to generate a list of randomly selected heads and tails values
+# determine how often a streak of 6 heads or 6 tails in a randomly generated list of heads and tails.
+# all of the code should loop to generate 10,000 times so we can find out what percentage of 
+# coin flips contain a streak of six heads or tails in a row.
+# use random.randint(0,1) 
+
+# template code 
+"""
+import random 
+
+number_of_streaks = 0
+
+for experimentNumber in range(10000):
+    # Code creates a list of 100 heads or tails values
+    # Code checks to see if there is a streak of 6 heads or tails in a row
+    print ('Chance of streak: %s%%'(number_of_streaks / 100))
+
+"""
+
+
